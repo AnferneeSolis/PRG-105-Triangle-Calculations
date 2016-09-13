499e46aa1ed6e45378d626a87165ef43964fe866
AnferneeSolis <asolis@students.mchenry.edu>
2016-09-12 19:34:51 -0500

    Update Triangle Calculations.py

--- Triangle Calculations.py
+++ Triangle Calculations.py
@@ -1,16 +1,17 @@
-# numbers need to be given smallest to largest
+# the variable cannot be negative
 x = raw_input()
 y = raw_input()
 z = raw_input()
-
+#function
 def is_triangle():
-    if x + y > z:
-        print("yes.")
-    if x + z > y:
-        print("yes")
-    if y + z > x:
-        print("yes")
-    else:
+    if int(x) + int(y) < int(z):
         print("no.")
-# if any of the checks fail, then the sides do not create a triangle.
+    elif int(x) + int(z) < int(y):
+        print("no.")
+    elif int(y) + int(z) < int(x):
+        print("no.")
+    else:
+        print("yes.")
+
 is_triangle()
+#Will still be considered a triangle is the sides create a degerate triangle
